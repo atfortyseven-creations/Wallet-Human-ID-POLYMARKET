4edbf41dffc1e97f3dd72e76c73865648c7b3865
Admin <josejordan20222@gmail.com>
2026-01-25 01:27:03 +0000

    FIX: Node version, Typescript and Webpack config

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env.DATABASE_URL || "postgresql://[REDACTED_DB_USER]:[REDACTED_DB_PASS]@localhost:5432/dummy",
   },
 });
